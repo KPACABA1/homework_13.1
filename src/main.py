01aa3ee6e6b98c9a3aa1f04bfb6ff699bed9a9c6
groni <groninger.official@yandex.ru>
2024-04-03 05:04:42 +0500

    Создал 2 новых экземпляра 2 дочерних классов от класса Product

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from functions import reading_a_file
-from classes import Category, Product
+from classes import Category, Product, Smartphone, LawnGrass
 
 # Присваиваю первой категории из файла "products.json"(смартфонам) класс Category
 category_1 = Category(reading_a_file()[0]["name"], reading_a_file()[0]["description"], reading_a_file()[0]["products"])
@@ -16,3 +16,11 @@ category_2 = Category(reading_a_file()[1]["name"], reading_a_file()[1]["descript
 # Присваиваю 1 товару класс Product
 product_2_1 = Product(reading_a_file()[1]["products"][0]["name"], reading_a_file()[1]["products"][0]["description"],
                       reading_a_file()[1]["products"][0]["price"], reading_a_file()[1]["products"][0]["quantity"])
+
+# Создаю новый товар(Смартфоны) от родительского класса Product
+product_smartphone = Smartphone("Nokia", "Бронированный телефон", 12000, 20,
+                                "2 GB", "SX4", "32 GB", "Black")
+
+# Создаю ещё 1 новый товар(Газонная трава) от родительского класса Product
+product_lawn_grass = LawnGrass("White clover", "Один из самых лучших сортов газонной травы", 2500,
+                               13, "Russia", "13 дней", "Green")
